bcd0f17f58fa1b73ac760278fcd8316350ad0763
AstereY <pavel.chmykhov@nutnet.ru>
2021-10-01 12:56:56 +0400

    update, now key is string

--- test_vernam.py
+++ test_vernam.py
@@ -1,24 +1,45 @@
 import time
 import random
+import itertools
 
 
-def key_generator():
+def wordGenerator(len):
+    iterable = ''.join(chr(x) for x in range(256)).encode('utf-16', 'surrogatepass').decode('utf-16', 'ignore')
+    yield from map(''.join, itertools.product(iterable, repeat=len))
+
+
+def key_generator(len):
     random.seed(time.time())
-    key = random.randint(0, 1113993)
+    key = ''
+    for i in range(len):
+        key += chr(random.randint(0, 0x10ffff))
 
     return key
 
 
-def vernam(text, key):
-    text = str(text)
-    text = text.replace(' ', '')
-    ciphertext = ''
-    for x in range(len(text)):
-        simb = ord(text[x])
-        simb = (simb + key)
-        ciphertext += chr(simb)
+def vernam(text, key, mode):
+    if mode == 'en':
+        text = str(text)
+        text = text.replace(' ', '')
+        ciphertext = ''
+        for x in range(len(text)):
+            simb = ord(text[x])
+            keysimb = ord(key[x])
+            simb = (simb + keysimb)
+            ciphertext += chr(simb)
 
-    return ciphertext
+        return ciphertext
+    if mode == 'de':
+        text = str(text)
+        text = text.replace(' ', '')
+        ciphertext = ''
+        for x in range(len(text)):
+            simb = ord(text[x])
+            keysimb = -1 * ord(key[x])
+            simb = (simb + keysimb)
+            ciphertext += chr(simb)
+
+        return ciphertext
 
 
 if __name__ == '__main__':
@@ -27,47 +48,34 @@ if __name__ == '__main__':
     if str.lower(mode) == 'en' or str.lower(mode) == 'encrypt':
 
         plaintext = input('text: ')
-        in_key = input('key, int(0, 1113993) (random if not filled): ')
-
-        try:
-            in_key = int(in_key)
-        except ValueError:
-            in_key = key_generator()
-            print('key= %s' % in_key)
-
-        in_key = int(in_key)
-        print(vernam(plaintext, in_key))
+        in_key = input('key, same length as text (random if not filled): ')
+        if not in_key:
+            in_key = key_generator(len(plaintext))
+            print('key is %s' % in_key)
+        if not plaintext:
+            print('error, no data!')
+        else:
+            print(vernam(plaintext, in_key, 'en'))
 
     elif str.lower(mode) == 'de' or str.lower(mode) == 'decrypt':
 
         ciphertext = input('ciphertext: ')
         in_key = input('key: ')
-
-        try:
-            in_key = int(in_key)
-        except ValueError:
-            print('key should be integer!')
-            in_key = key_generator()
-            print('key = %s' % in_key)
-
-        in_key = int(in_key)
-        in_key = (-1) * in_key
-        print(vernam(ciphertext, in_key))
+        if not (ciphertext or in_key):
+            print('error, no data!')
+        else:
+            print(vernam(ciphertext, in_key, 'de'))
 
     elif str.lower(mode) == 'ca' or str.lower(mode) == 'cryptoanalysis':
         ciphertext = input('ciphertext: ')
-        key = int(input('key: '))
-        awaitedtext = vernam(ciphertext, -1 * key)
-        for i in range(1113994):
-            possible_key = -1 * i
-            if awaitedtext == vernam(ciphertext, possible_key):
-                print('found your text in %d iterations' % i)
-                print(vernam(ciphertext, possible_key))
-                print('(without key it wouldn\'t be possible)')
+        key = input('key: ')
+        awaitedtext = vernam(ciphertext, key, 'de')
+
+        for i in wordGenerator(len(ciphertext)):
+            print(i)
+            if i == awaitedtext:
+                print('Text found!')
                 break
-            else:
-                # print('possible text: %s' % vernam(ciphertext, possible_key).encode('utf-16', 'surrogatepass').decode('utf-16', 'ignore'))
-                pass
 
     else:
         print('wrong cmd')
